29bd78a2966ddc5796d4b08b289fab491c95c5d4
Hudraulic <aggravated1@gmail.com>
2021-01-31 09:51:10 +0300

    First task

---
+++ VK_GeekBrains_MyApp/VK_GeekBrains_MyApp/Model/Group.swift
@@ -0,0 +1,18 @@
+//
+//  Group.swift
+//  VK_GeekBrains_MyApp
+//
+//  Created by Владислав Капиносов on 19.01.2021.
+//
+
+import UIKit
+
+struct Group {
+    
+    let name: String
+    let icon: UIImage
+    let id: Int
+    
+}
+
+
